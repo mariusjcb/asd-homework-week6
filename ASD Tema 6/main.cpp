0d80383a2a780437d3656ca58d217b696d63b9cb
Marius Ilie <marius_ilie20082001@yahoo.com>
2017-01-05 20:20:22 +0200

    Initial Commit

---
+++ ASD Tema 6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASD Tema 6
+//
+//  Created by Marius Ilie on 05/01/17.
+//  Copyright © 2017 University of Bucharest - Marius Ilie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
